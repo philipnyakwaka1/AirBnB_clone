4de99cc23c8e7194fffb4e8fd657a7a7076387aa
AustinRado <radoaussy@gmail.com>
2023-10-15 14:14:09 -0700

    console

--- console.py
+++ console.py
@@ -20,6 +20,27 @@ class HBNBCommand(cmd.Cmd):
         """Empty line should not print anything"""
         pass
 
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it, and prints the id"""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        class_name = arg.split()[0]
+        valid_classes = ["BaseModel", "User", "State", "City", "Amenity", "Place", "Review"]
+
+        if class_name not in valid_classes:
+            print("** class doesn't exist **")
+            return
+        
+        class_to_create = models.classes.get(class_name)
+        if class_to_create:
+            new_instance = class_to_create()
+            new_instance.save()
+            print(new_instance.id)
+        else:
+            print("** class doesn't exist **")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
